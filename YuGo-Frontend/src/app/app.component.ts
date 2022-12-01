8438a91908e34de18aa3444031d84f2c905ecbf2
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-12-01 14:09:26 +0100

    Added Navbar

--- YuGo-Frontend/src/app/app.component.ts
+++ YuGo-Frontend/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../styles.css','./app.component.css']
 })
 export class AppComponent {
-  title = 'YuGo-Frontend';
+  title = 'YuGo';
 
-  constructor(public dialog: MatDialog) {}
-
-  openDialog() {
-    this.dialog.open(LoginComponent);
-  }
 }
--- YuGo-Frontend/src/app/app.module.ts
+++ YuGo-Frontend/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,9 @@ import {MatDialogModule} from '@angular/material/dialog';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
