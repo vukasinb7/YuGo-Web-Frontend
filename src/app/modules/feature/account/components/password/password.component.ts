389d4551199c4b87f94e91078eecf2742e883cbe
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-01-05 17:46:14 +0100

    Password change implemented

--- src/app/modules/feature/account/components/password/password.component.ts
+++ src/app/modules/feature/account/components/password/password.component.ts
@@ -1,27 +1,69 @@
-import {Component, Input} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AfterViewInit, Component, Input} from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
 import {UserService} from "../../../../shared/services/user.service";
-import {AuthService} from "../../../../core/services/auth.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {Observable, of, take} from "rxjs";
+import {AuthService} from "../../../../core/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
   styleUrls: ['./password.component.css']
 })
-export class PasswordComponent {
+export class PasswordComponent{
   @Input()
   public userId: number = -1;
   passwordForm : FormGroup;
-  editEnabled: boolean
-  constructor(private _userService : UserService) {
+  editEnabled: boolean;
+
+  errorMessage:string = '';
+  constructor(private _userService : UserService, private _authService: AuthService, private _router: Router) {
     this.passwordForm = new FormGroup({
-      oldPassword: new FormControl('', [Validators.required]),
-      newPassword: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required]),
+      currentPassword: new FormControl('', [Validators.required]),
+      newPassword: new FormControl('', [Validators.required, this.passwordValidator()]),
+      confirmPassword: new FormControl('', [Validators.required], [this.confirmPasswordValidator()]),
     });
     this.passwordForm.disable();
     this.editEnabled = false;
+  }
+
+  private passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if(control.value.length < 6){
+        return {minLength:{value:control.value}};
+      }
+      if(control.value.length > 20){
+        return {maxLength:{value:control.value}};
+      }
+      let whiteSpaceRegex:RegExp = new RegExp("^(?!.* ).{6,20}$")
+      if(!whiteSpaceRegex.test(control.value)){
+        return {whitespace:{value:control.value}};
+      }
+      return null;
+    };
+  }
+
+  private confirmPasswordValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return of((() => {
+        const passwordField = this.passwordForm.controls['newPassword'];
+        if(passwordField?.valid){
+          if(passwordField.value != control.value){
+            return {passwordsNotMatching:{value:control.value}};
+          }
+        }
+        return null;
+      })());
+    };
   }
 
   enableEdit() : void{
@@ -32,29 +74,41 @@ export class PasswordComponent {
   cancelEdit() : void{
     this.passwordForm.disable();
     this.editEnabled = false;
+    this.passwordForm.reset();
+    this.errorMessage = "";
   }
 
   submitEdit() : void {
-    if (this.passwordForm.controls['confirmPassword'].value != this.passwordForm.controls['newPassword'].value || this.passwordForm.invalid){
+    if (this.passwordForm.invalid){
 
     }
     else {
       this._userService.changePassword(this.userId, {
-        "oldPassword": this.passwordForm.controls['oldPassword'].value,
+        "oldPassword": this.passwordForm.controls['currentPassword'].value,
         "newPassword": this.passwordForm.controls['newPassword'].value
-      }).subscribe({
+      }).pipe(take(1)).subscribe({
         next: (info) => {
+          this.cancelEdit();
+          if (this.userId == this._authService.getId()){
+            localStorage.removeItem('user');
+            this._authService.setUser();
+            this._router.navigate(['/']);
+            this._authService.logOut().subscribe({
+              next: (result) => {
 
+              },
+              error: (error) => {
+                console.log(error);
+              },
+            });
+          }
         },
         error: (error) => {
           if (error instanceof HttpErrorResponse) {
-
+            this.errorMessage = error.error.message;
           }
         }
       })
-
-      this.passwordForm.disable();
-      this.editEnabled = false;
     }
   }
 }
